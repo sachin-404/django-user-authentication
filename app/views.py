dd384c1fc78808abd55e97c55f1bc65fc3b83c57
Sachin <thesachinkant404@gmail.com>
2023-04-27 01:03:49 +0530

    user login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate
 from django.contrib import messages
 
 # Create your views here.
@@ -22,5 +23,15 @@ def signup(request):
             return redirect('login')
     return render(request, 'signup.html')
 
+# User Login
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "Username or Password is incorrect!!")
     return render(request, 'login.html')

a94dcce27b090f1505885f0886a90edbf98e4021
Sachin <thesachinkant404@gmail.com>
2023-04-27 01:55:28 +0530

    home page

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
-    path('signup/', views.signup, name = 'signup'),
-    path('', views.login, name = 'login'),
+    path('signup/', views.signupPage, name = 'signup'),
+    path('', views.loginPage, name = 'login'),
+    path('logout/', views.logoutPage, name = 'logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User 
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='login')
 def home(request):
     return render(request, 'home.html')
 
 # User Registration
-def signup(request):
+def signupPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
@@ -24,7 +27,7 @@ def signup(request):
     return render(request, 'signup.html')
 
 # User Login
-def login(request):
+def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -35,3 +38,8 @@ def login(request):
         else:
             messages.success(request, "Username or Password is incorrect!!")
     return render(request, 'login.html')
+
+def logoutPage(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out!!")
+    return redirect('login')
